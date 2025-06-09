8df47c43c33c74cfe1ddd434c0b7222b1801138f
Denalia Zhi <d.naliazhi@gmail.com>
2025-06-09 18:39:36 -0400

    Disable background color change for container

--- script.js
+++ script.js
@@ -33,8 +33,10 @@ for (let i = 0; i < numSquares; i++) {
 
 // Track mouse over grid
 container.addEventListener("mouseover", (e) => {
-    let red = Math.random() * 255;
-    let green = Math.random() * 255;
-    let blue = Math.random() * 255;
-    e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    if (e.target.className === "pixel") {
+        let red = Math.random() * 255;
+        let green = Math.random() * 255;
+        let blue = Math.random() * 255;
+        e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    }
 })
