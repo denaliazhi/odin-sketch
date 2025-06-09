04734ec9b61a4fca29d337a03aa0ea8accd07a33
Denalia Zhi <d.naliazhi@gmail.com>
2025-06-09 18:10:35 -0400

    Change grid dimensions based on user input

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-const gridSize = 16;
+const GRID_SIZE = 960; // Pixels per side
 const container = document.querySelector(".container");
 
+let numSquares;
+let keepAsking = true;
+
+// Prompt user for dimensions of grid
+while (keepAsking) {
+    numSquares = +prompt("# of squares per side: ", "16 - 100");
+    if (Number.isInteger(numSquares) 
+        && numSquares >= 16
+        && numSquares <= 100 ) 
+    {
+        keepAsking = false;
+    } else {
+        alert("Invalid input. Please input a number in the range: 16 - 100")
+    }
+}
+
+// Calculate pixels per side of square
+let squareWidth = GRID_SIZE / numSquares;
+
 // Load starter grid
-for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
+container.style.maxWidth = `${GRID_SIZE}px`;
+for (let i = 0; i < numSquares; i++) {
+    for (let j = 0; j < numSquares; j++) {
         const square = document.createElement("div");
         square.classList.add("pixel");
+        square.style.height = square.style.width = `${squareWidth}px`;
         container.appendChild(square);
     }
 }
